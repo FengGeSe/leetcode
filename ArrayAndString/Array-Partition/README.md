3e1531ac3e4e43e8bc7efed7903318c344df566f
吴帅帅 <2766705651@qq.com>
2019-04-25 11:05:44 +0800

    update

--- ArrayAndString/Array-Partition/README.md
+++ ArrayAndString/Array-Partition/README.md
@@ -18,3 +18,33 @@ Explanation:
 ​	n is 2, and the maximum sum of pairs is 4 = min(1, 2) + min(3, 4).
 
 
+
+**Note:**
+
+1. **n** is a positive integer, which is in the range of [1, 10000].
+2. All the integers in the array will be in the range of [-10000, 10000].
+
+
+
+```
+func arrayPairSum(nums []int) int {
+    
+		var backet [20001]int
+	for _, val := range nums {
+		backet[10000+val]++
+	}
+	count := 0
+	sum := 0
+	for index := 0; index < 20001; index++ {
+		for backet[index] > 0 {
+			count++
+			if count&1 == 1 {
+				sum += index - 10000
+			}
+			backet[index]--
+		}
+	}
+    return sum
+}
+```
+
--- ArrayAndString/Array-Partition/main.go
+++ ArrayAndString/Array-Partition/main.go
@@ -3,20 +3,39 @@ package main
 import ()
 
 func ArrayPartition(nums []int) int {
-	var (
-		sum  = 0
-		flag = false
-	)
-	for i := 0; i < len(nums); i++ {
-		for j := 1; j < len(nums)-i; j++ {
-			if nums[j-1] > nums[j] {
-				nums[j-1], nums[j] = nums[j], nums[j-1]
-			}
-		}
-		if flag {
-			sum += nums[len(nums)-i-1]
-		}
-		flag = !flag
+	QuickSort(nums)
+	sum := 0
+	for i := 0; i < len(nums); i += 2 {
+		sum += nums[i]
 	}
 	return sum
+}
+
+// 快排
+func QuickSort(nums []int) {
+	if len(nums) <= 1 {
+		return
+	}
+
+	var (
+		key   = nums[0]
+		left  = 0
+		right = len(nums) - 1
+	)
+
+	for left < right {
+		for left < right && nums[right] > key {
+			right--
+		}
+		nums[left] = nums[right]
+
+		for left < right && nums[left] <= key {
+			left++
+		}
+		nums[right] = nums[left]
+	}
+
+	nums[left] = key
+	QuickSort(nums[:left])
+	QuickSort(nums[left+1:])
 }
